86cebb054fc20b747a572cf54b2a8d269e9555b6
Diana Cardona <dscardona@gmail.com>
2023-05-24 15:11:53 -0400

    Set up turtle, draw one dot

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # import colorgram
+import turtle as t
+import tkinter as tk
+from tkinter import ttk
+
+cursor = t.Turtle()
+t.colormode(255)
+cursor.speed(0)
 
 # rgb_colors = []
 # colors = colorgram.extract("hirst-dots.jpg", 30)
@@ -10,3 +17,10 @@
 #     rgb_colors.append(new_color)
 
 color_list = [(212, 149, 95), (215, 80, 62), (47, 94, 142), (231, 218, 92), (148, 66, 91), (22, 27, 40), (155, 73, 60), (122, 167, 195), (40, 22, 29), (39, 19, 15), (209, 70, 89), (192, 140, 159), (39, 131, 91), (125, 179, 141), (75, 164, 96), (229, 169, 183), (15, 31, 22), (51, 55, 102), (233, 220, 12), (159, 177, 54), (99, 44, 63), (35, 164, 196), (234, 171, 162), (105, 44, 39), (164, 209, 187), (151, 206, 220)]
+
+cursor.width(20)
+dot = cursor.forward(1)
+
+
+screen = t.Screen()
+screen.exitonclick()
