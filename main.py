7b78938d60cba40b407b33e156ebeb667b306cfb
Diana Cardona <dscardona@gmail.com>
2023-05-26 16:29:09 -0400

    Change function into for loop, hide turtle

--- main.py
+++ main.py
@@ -20,14 +20,19 @@ cursor.speed(0)
 
 color_list = [(212, 149, 95), (215, 80, 62), (47, 94, 142), (231, 218, 92), (148, 66, 91), (22, 27, 40), (155, 73, 60), (122, 167, 195), (40, 22, 29), (39, 19, 15), (209, 70, 89), (192, 140, 159), (39, 131, 91), (125, 179, 141), (75, 164, 96), (229, 169, 183), (15, 31, 22), (51, 55, 102), (233, 220, 12), (159, 177, 54), (99, 44, 63), (35, 164, 196), (234, 171, 162), (105, 44, 39), (164, 209, 187), (151, 206, 220)]
 
+cursor.setheading(225)
+cursor.penup()
+cursor.hideturtle()
+cursor.forward(300)
+cursor.setheading(0)
 
-for _ in range(10):
-    cursor.color(random.choice(color_list))
-    cursor.width(20)
-    cursor.forward(1)
-    cursor.penup()
+
+for dot_count in range(10):
+    cursor.dot(20,random.choice(color_list))
     cursor.forward(50)
-    cursor.pendown()
+
+
+
 
 
 
